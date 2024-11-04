036293a9d3fafdf2807c2ec4b71708c8fe47d25c
Belal Elshenety <belal.elshenetyl@gmail.com>
2024-11-03 23:51:11 -0800

    updated more of the outdated tensorflow version 1 code

--- learn/ngram.py
+++ learn/ngram.py
@@ -1,166 +1,110 @@
-from collections import Counter
 import random
+import pickle
+import json
+import numpy as np
+from collections import Counter
+import argparse
+from math import log, exp
+
 
 class NgramSequence:
     def __init__(self, chart_notes):
         self.sequence = [sym for _, _, _, sym in chart_notes]
 
     def get_ngrams(self, k, pre=True, post=True):
-        prepend = []
-        if pre:
-            prepend = ['<pre{}>'.format(i) for i in reversed(list(range(k - 1)))]
-
-        append = []
-        if post:
-            append = ['<post>']
-
+        prepend = ['<pre{}>'.format(i) for i in reversed(range(k - 1))] if pre else []
+        append = ['<post>'] if post else []
         sequence = prepend + self.sequence + append
-        for i in range(len(sequence) - (k - 1)):
-            yield tuple(sequence[i:i + k])
+        return (tuple(sequence[i:i + k]) for i in range(len(sequence) - k + 1))
+
 
 class NgramLanguageModel:
     def __init__(self, k, ngram_counts):
         self.k = k
         self.ngram_counts = ngram_counts
-        self.ngram_total = sum(self.ngram_counts.values())
-
-        self.history_counts = Counter()
-        for ngram, count in list(ngram_counts.items()):
-            self.history_counts[ngram[:-1]] += count
-
-        self.vocab = set()
-        for ngram, _ in list(ngram_counts.items()):
-            for w in ngram:
-                self.vocab.add(w)
+        self.history_counts = Counter({ngram[:-1]: count for ngram, count in ngram_counts.items()})
+        self.vocab = set(w for ngram in ngram_counts for w in ngram)
+        self.vocab_size = len(self.vocab)
 
     def mle(self, ngram):
-        ngram_count = self.ngram_counts[ngram]
-        history = ngram[:-1]
-        history_count = self.history_counts[ngram[:-1]]
-        #print ngram, ngram_count, history_count
-        return float(ngram_count) / history_count
+        return self.ngram_counts[ngram] / self.history_counts[ngram[:-1]]
 
     def laplace(self, ngram, smooth=1):
-        history_count = self.history_counts.get(ngram[:-1], 0)
-        card_v = len(self.vocab) + 1
-        numerator = 1 + self.ngram_counts.get(ngram, 0)
-        denominator = card_v + history_count
-
-        return float(numerator) / denominator
+        numerator = self.ngram_counts.get(ngram, 0) + smooth
+        denominator = self.history_counts.get(ngram[:-1], 0) + self.vocab_size + smooth
+        return numerator / denominator
 
     def generate(self, history, strategy='argmax'):
         if strategy == 'argmax':
-            highest_prob = 0
-            best_chars = [None]
-            for v in self.vocab:
-                v_prob = self.laplace(history + (v,))
-                if v_prob > highest_prob:
-                    highest_prob = v_prob
-                    best_chars = [v]
-                elif v_prob == highest_prob:
-                    best_chars.append(v)
-            return random.choice(best_chars)
-        else:
-            raise NotImplementedError()
+            max_prob = max(self.laplace(history + (v,)) for v in self.vocab)
+            candidates = [v for v in self.vocab if self.laplace(history + (v,)) == max_prob]
+            return random.choice(candidates)
+        raise NotImplementedError()
+
+
+def train_ngram_model(dataset_files, k, diff_filter, model_fp):
+    ngram_counts = Counter()
+    for file in dataset_files:
+        with open(file, 'r') as f:
+            song_meta = json.load(f)
+        for chart in song_meta['charts']:
+            if diff_filter and diff_filter != chart['difficulty_coarse']:
+                continue
+            for ngram in NgramSequence(chart['notes']).get_ngrams(k):
+                ngram_counts[ngram] += 1
+
+    model = NgramLanguageModel(k, ngram_counts)
+    with open(model_fp, 'wb') as f:
+        pickle.dump(model, f)
+
+
+def evaluate_ngram_model(dataset_files, model_fp, k, diff_filter):
+    with open(model_fp, 'rb') as f:
+        model = pickle.load(f)
+
+    entropies, accuracies = [], []
+    for file in dataset_files:
+        with open(file, 'r') as f:
+            song_meta = json.load(f)
+        for chart in song_meta['charts']:
+            if diff_filter and diff_filter != chart['difficulty_coarse']:
+                continue
+
+            log_prob, hits, n = 0.0, 0, 0
+            for ngram in NgramSequence(chart['notes']).get_ngrams(k, pre=True, post=False):
+                generated = model.generate(ngram[:-1])
+                if generated == ngram[-1]:
+                    hits += 1
+                log_prob += log(model.laplace(ngram))
+                n += 1
+
+            entropies.append(-log_prob / n)
+            accuracies.append(hits / n)
+
+    perplexities = np.exp(entropies)
+    results = { 
+        'cross_entropy': (np.mean(entropies), np.std(entropies)),
+        'perplexity': (np.mean(perplexities), np.std(perplexities)),
+        'accuracy': (np.mean(accuracies), np.std(accuracies))
+    }
+
+    for metric, (mean, std) in results.items():
+        print(f"{metric.capitalize()}: Mean={mean:.4f}, Std={std:.4f}")
+
 
 if __name__ == '__main__':
-    import argparse
-    from collections import Counter
-    import pickle as pickle
-    import json
-    import math
-    import os
-    import numpy as np
-
     parser = argparse.ArgumentParser()
-    parser.add_argument('dataset_fp', type=str, help='')
-    parser.add_argument('model_fp', type=str, help='')
-    parser.add_argument('--k', type=int, help='')
-    parser.add_argument('--diff', type=str, help='')
-    parser.add_argument('--task', type=str, choices=['train', 'eval'], help='')
-
-    parser.set_defaults(
-        k=1,
-        task='train',
-        diff='')
+    parser.add_argument('dataset_fp', type=str, help='Path to dataset file')
+    parser.add_argument('model_fp', type=str, help='Path to save/load the model')
+    parser.add_argument('--k', type=int, default=1, help='N-gram size')
+    parser.add_argument('--diff', type=str, help='Difficulty filter')
+    parser.add_argument('--task', type=str, choices=['train', 'eval'], default='train', help='Task to perform')
 
     args = parser.parse_args()
-
     with open(args.dataset_fp, 'r') as f:
-        json_fps = f.read().split()
+        dataset_files = f.read().split()
 
     if args.task == 'train':
-        ncharts = 0
-        ngram_counts = Counter()
-
-        for json_fp in json_fps:
-            with open(json_fp, 'r') as f:
-                song_meta = json.loads(f.read())
-
-            for chart_meta in song_meta['charts']:
-                if args.diff and args.diff != chart_meta['difficulty_coarse']:
-                    continue
-                chart_sequence = NgramSequence(chart_meta['notes'])
-                for ngram in chart_sequence.get_ngrams(args.k):
-                    ngram_counts[ngram] += 1
-                ncharts += 1
-
-        model = NgramLanguageModel(args.k, ngram_counts)
-
-        ptotmle = 0.0
-        for ngram, count in list(ngram_counts.items()):
-            ptotmle += model.mle(ngram)
-
-        with open(args.model_fp, 'wb') as f:
-            pickle.dump(model, f)
-
+        train_ngram_model(dataset_files, args.k, args.diff, args.model_fp)
     elif args.task == 'eval':
-        with open(args.model_fp, 'rb') as f:
-            model = pickle.load(f)
-
-        chart_entropies = []
-        chart_accuracies = []
-        for json_fp in json_fps:
-            with open(json_fp, 'r') as f:
-                song_meta = json.loads(f.read())
-            #print song_meta['title']
-
-            for chart_meta in song_meta['charts']:
-                if args.diff and args.diff != chart_meta['difficulty_coarse']:
-                    continue
-                chart_sequence = NgramSequence(chart_meta['notes'])
-                chart_log_prob = 0.0
-                chart_n = 0
-                hits = 0
-                for ngram in chart_sequence.get_ngrams(args.k, pre=True, post=False):
-                    generated = model.generate(ngram[:-1])
-                    actual = ngram[-1]
-                    if generated == actual:
-                        hits += 1
-                    ngram_prob = model.laplace(ngram)
-                    #print '{}: {}'.format(ngram, ngram_prob)
-                    chart_log_prob += np.log(ngram_prob)
-                    chart_n += 1
-                cross_entropy = (-1.0 / chart_n) * chart_log_prob
-                chart_entropies.append(cross_entropy)
-                chart_accuracies.append(float(hits) / chart_n)
-
-        chart_entropies = np.array(chart_entropies)
-        chart_perplexities = np.exp(chart_entropies)
-        #print chart_entropies
-        #print chart_perplexities
-        #print chart_entropies
-        #print chart_perplexities
-        #print chart_accuracies
-        #print 'Cross-entropy (nats): {}, std {}'.format(np.mean(chart_entropies), np.std(chart_entropies))
-        #print 'Perplexity: {}, std {}'.format(np.mean(chart_perplexities), np.std(chart_perplexities))
-        #print 'Accuracy: {}, std {}'.format(np.mean(chart_accuracies), np.std(chart_accuracies))
-        #print '-' * 30 + 'COPY PASTA' + '-' * 30
-        eval_funcs = [np.mean, np.std, np.min, np.max]
-        eval_results = [[f(x) for f in eval_funcs] for x in [chart_entropies, chart_perplexities, chart_accuracies]]
-        copy_pasta = []
-        for l in eval_results:
-            copy_pasta.append(','.join([str(x) for x in l]))
-        print(','.join(copy_pasta))
-    else:
-        raise NotImplementedError()
+        evaluate_ngram_model(dataset_files, args.model_fp, args.k, args.diff)
